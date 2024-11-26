eb93758536a85844b2840b9f1b64095bbef1f8a3
Mark Harmsen <mharmsenr@gmail.com>
2024-11-25 22:41:11 -0500

    transaction_table

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,11 @@
 class IndexController < ApplicationController
     def home; end
-    def transaction_table; end
+
+    def transaction_table
+        @transactions = Transaction.all
+        puts(@transactions.count)
+    end
+
     def email_form; end
     def chatgpt_connection; end
 end
