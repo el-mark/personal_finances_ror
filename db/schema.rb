2db7a9a80567cad3d6db953890a4d324a6789a42
Mark Harmsen <mharmsenr@gmail.com>
2024-11-26 10:29:55 -0500

    books scafold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_25_185921) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_26_152537) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "author"
+    t.string "title"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "email_transactions", force: :cascade do |t|
     t.integer "amount"
