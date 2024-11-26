d34c113c1ccb47b0b71e65bba74ea33be0ef59cd
Mark Harmsen <mharmsenr@gmail.com>
2024-11-25 19:19:32 -0500

    bootstrap template

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,8 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "color-modes", to: "color-modes.js"
+pin "bootstrap", to: "bootstrap.bundle.min.js"
+pin "dashboard", to: "dashboard.js"
+
 pin_all_from "app/javascript/controllers", under: "controllers"
