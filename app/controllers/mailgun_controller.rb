f35b52a63f4ae9dffff182af0e365e09f1b71618
Mark Harmsen <mharmsenr@gmail.com>
2024-12-03 10:59:22 -0500

    fix raw_message field error

--- app/controllers/mailgun_controller.rb
+++ app/controllers/mailgun_controller.rb
@@ -4,7 +4,6 @@ class MailgunController < ApplicationController
     def forward_email
         email_params = JSON.parse(params.to_json)
         email = Email.create(
-            raw_message: params,
             json_raw_message: email_params,
             sender: email_params['sender'],
             body: email_params['body-html'],
