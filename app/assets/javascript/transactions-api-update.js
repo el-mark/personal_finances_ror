b570a7dd683e47dddbc6caad62cb720243692f63
Mark Harmsen <mharmsenr@gmail.com>
2024-12-20 11:28:10 -0500

    change_frequency

--- app/assets/javascript/transactions-api-update.js
+++ app/assets/javascript/transactions-api-update.js
@@ -1,6 +1,7 @@
 document.addEventListener('turbo:load', () => {
     // console.log('Turbo load event triggered');
     change_category();
+    change_frequency();
 });
 
 function change_category() {    
@@ -15,6 +16,28 @@ function change_category() {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({ transaction: { category: value } }),
+            })
+            .then(response => response.json())
+            .then(data => console.log('Success:', data))
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        });
+    });
+}
+
+function change_frequency() {    
+    document.querySelectorAll('.change-frequency').forEach(function(element) {
+        element.addEventListener('change', function() {
+            const transactionId = element.getAttribute('data');
+            console.log(transactionId);
+            const value = this.value;
+            fetch(`/transactions/${transactionId}/api_update`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ transaction: { frequency: value } }),
             })
             .then(response => response.json())
             .then(data => console.log('Success:', data))
