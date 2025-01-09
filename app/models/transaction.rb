6814f182c41501da9bd9446025df73f6ea693569
Mark Harmsen <mharmsenr@gmail.com>
2025-01-09 11:11:32 -0500

    new categories

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
     enum :currency, [ :no_currency, :usd, :pen ]
     enum :frequency, [ :common, :rare ]
-    enum :category, [ :other_category, :supermarket, :transference, :health, :investment, :entertainment, :home ]
+    enum :category, [ :other_category, :supermarket, :transference, :health, :investment, :entertainment, :home, :services, :transport, :pet, :charity, :debts, :delivery, :subscriptions, :clothing, :education, :travel ]
 
     belongs_to :email, optional: true
     belongs_to :user
