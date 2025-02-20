058af9be7c670853dd29b9226af4ec623e9e4b07
Mark Harmsen <mharmsenr@gmail.com>
2025-02-20 09:42:02 -0500

    rollback migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_18_151822) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_18_150454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -70,7 +70,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_18_151822) do
     t.datetime "remember_created_at"
     t.string "provider"
     t.string "uid"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
