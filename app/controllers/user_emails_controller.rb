23725f36aaa34f6996af18c7f529d98b52aa59eb
Mark Harmsen <mharmsenr@gmail.com>
2025-02-21 17:15:33 -0500

    add new user_email feature

--- app/controllers/user_emails_controller.rb
+++ app/controllers/user_emails_controller.rb
@@ -2,4 +2,21 @@ class UserEmailsController < ApplicationController
     def new
         @user_email = UserEmail.new
     end
+
+    def create
+        @user_email = UserEmail.new(user_email_params)
+        @user_email.user_id = current_user.id
+
+        if @user_email.save
+            redirect_to user_emails_path, notice: "El correo fue agregado con éxito."
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_email_params
+        params.require(:user_email).permit(:address)
+    end
 end
